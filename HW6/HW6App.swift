f58f11ae38545dfcd849fe778cd2683bcd47ad0e
armanjakhayev <67386397+armanjakhayev@users.noreply.github.com>
2021-12-11 12:24:10 -0800

    Initial Commit

---
+++ HW6/HW6App.swift
@@ -0,0 +1,17 @@
+//
+//  HW6App.swift
+//  HW6
+//
+//  Created by Arman on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HW6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
